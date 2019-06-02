f1acca7ffb9027ae97c3919e33f1606baffcb706
reslie <1550167691@qq.com>
2019-06-02 15:07:36 +0800

    add koa

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
 
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.hello = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!!!!!!!!!!!!");
-});
+// exports.hello = functions.https.onRequest((request, response) => {
+//   response.send("Hello from Firebase!!!!!!!!!!!!");
+// });
+exports.app = functions.https.onRequest(app);
